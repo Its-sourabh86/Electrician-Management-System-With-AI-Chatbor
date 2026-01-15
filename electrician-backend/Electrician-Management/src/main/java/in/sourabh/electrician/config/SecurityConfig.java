ca45eed11c678c1bbb215b43e6dd3e8503a78e3c
Its-sourabh86 <sourabhpa787987@gmail.com>
2026-01-15 23:38:01 +0530

    Fix CORS for Vercel frontend

--- electrician-backend/Electrician-Management/src/main/java/in/sourabh/electrician/config/SecurityConfig.java
+++ electrician-backend/Electrician-Management/src/main/java/in/sourabh/electrician/config/SecurityConfig.java
@@ -54,7 +54,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration
-                .setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:3000"));
+                .setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:3000",
+                    "https://electrician-anagement-system-with-a-psi.vercel.app"
+                ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
